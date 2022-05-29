82378069a80ce5b5ec259170f38f98df0c013e52
redst4r <redst4r@web.de>
2022-05-29 14:53:20 -0700

    added seqeuence encoding, decoding + tests

--- src/utils.rs
+++ src/utils.rs
@@ -171,3 +171,98 @@ pub fn write_to_csv(df_final: &mut DataFrame, output_csv_file: String){
         .finish(df_final)
         .unwrap();    
 }
+
+pub fn seq_to_int(seq: String) -> u64{
+
+    assert!(seq.len() <= 32); // cant handle longer sequences in a single 64bit integer!
+    let s: String = seq
+    .chars()
+    .map(|x| match x {
+        'A' => '0',
+        'C' => '1',
+        'G' => '2',
+        'T' => '3',
+        _ => panic!("unkown seq character"),
+    })
+    .collect();
+    let int_seq = u64::from_str_radix(&s, 4).unwrap();
+    int_seq
+}
+
+
+pub fn int_to_seq(i: u64, seq_len:u64) -> String{
+
+    let mut q = i;
+    let mut result: Vec<u64> = Vec::new();
+    while q>= 4{
+        let quotient = q / 4;
+        let remainder = q % 4;
+        result.push(remainder);
+        q = quotient;
+    }
+    result.push(q);
+
+    while result.len() < seq_len as usize{
+        result.push(0);
+
+    }
+
+    result.reverse();
+
+    let s: String = result.iter()
+    .map(|x| match x {
+        0 => 'A',
+        1 => 'C',
+        2 => 'G',
+        3 => 'T',
+        _ => panic!("unkown seq character"),
+    })
+    .collect::<String>();
+
+    // println!("{:?}", s);
+    s
+}   
+
+
+#[cfg(test)]
+mod tests {
+    // use std::io::W/rite;
+    // use crate::bus::{BusRecord, BusHeader, CellIterator, BusIteratorBuffered, BusWriter};
+    use crate::utils::*;
+
+    #[test]
+    fn encode_decode_seq(){
+        let x = vec![0,1,2,3,4,5,6,7,8];
+        for i in x{
+            println!("DECDODED {}:{:?}", i, int_to_seq(i, 5));
+        }
+
+
+        //  base order
+        assert_eq!(int_to_seq(0, 1), "A");
+        assert_eq!(int_to_seq(1, 1), "C");
+        assert_eq!(int_to_seq(2, 1), "G");
+        assert_eq!(int_to_seq(3, 1), "T");
+    // 
+        // # padding leading A's
+        assert_eq!(int_to_seq(0, 3), "AAA");
+        assert_eq!(int_to_seq(1, 3), "AAC");
+        assert_eq!(int_to_seq(2, 3), "AAG");
+        assert_eq!(int_to_seq(3, 3), "AAT");
+    // 
+        assert_eq!(int_to_seq(4, 2), "CA");
+        assert_eq!(int_to_seq(5, 2), "CC");
+        assert_eq!(int_to_seq(6, 2), "CG");
+        assert_eq!(int_to_seq(7, 2), "CT");
+    // 
+        assert_eq!(int_to_seq(148, 4), "GCCA");
+    // 
+        // # make sure to raise an error when the decoded string is actually longer
+        // # then requested (since its probably a bug in the code calling _decode_int_to_ACGT)
+        // with pytest.raises(AssertionError):
+            // busio._decode_int_to_ACGT(148, seq_len=2)
+        // with pytest.raises(AssertionError):
+            // busio._decode_int_to_ACGT(-1, seq_len=1)
+    }
+
+}
